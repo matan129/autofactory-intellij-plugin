a7889202d4b0277e5012ee2f71a493f4594e12b4
Matan Rosenberg <matan129@gmail.com>
2017-06-30 17:04:42 +0300

    Add documentation

--- src/main/java/mr/intellij/plugin/autofactory/augment/GuardedPsiAugmentProvider.java
+++ src/main/java/mr/intellij/plugin/autofactory/augment/GuardedPsiAugmentProvider.java
@@ -7,14 +7,25 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.List;
 
+/**
+ * PsiAugment provider that is stack overflow resistant.
+ *
+ * The rational for such class is that {@link InjectAugmentProvider} is supposed to (sometimes) add {@code @Inject}
+ * annotations on constructors, but in its {@code getAugments()} logic is check for other annotations presence, which
+ * triggers another {@code getAugments()} and so on, causing stack overflow.
+ *
+ * This class fixes this issue by introducing a {@link Suppressor} that disables further {@code getAugment()} calls.
+ *
+ * @param <T> The supported {@link PsiElement} (i.e. {@link com.intellij.psi.PsiAnnotation}).
+ */
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class GuardedPsiAugmentProvider<T extends PsiElement> extends PsiAugmentProvider {
 
-    private boolean suppress = false;
-
     private final Class<T> supportedType;
+    private boolean suppress = false;
 
     @NotNull
     @Override
@@ -24,7 +35,7 @@ public abstract class GuardedPsiAugmentProvider<T extends PsiElement> extends Ps
                                                                    @NotNull Class<Psi> type) {
 
         if (suppress || supportedType != type) {
-            return super.getAugments(element, type);
+            return Collections.emptyList();
         }
 
         try (Suppressor ignored = new Suppressor()) {
