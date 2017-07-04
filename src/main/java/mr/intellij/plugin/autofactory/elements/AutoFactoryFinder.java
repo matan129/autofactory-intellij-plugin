fa1a20fe98eda917b648b63ac1239b3466c89904
Matan Rosenberg <matan129@gmail.com>
2017-07-04 23:54:29 +0300

    Add PoC AutoFactoryFinder

--- src/main/java/mr/intellij/plugin/autofactory/elements/AutoFactoryFinder.java
+++ src/main/java/mr/intellij/plugin/autofactory/elements/AutoFactoryFinder.java
@@ -1,14 +1,17 @@
 package mr.intellij.plugin.autofactory.elements;
 
+import com.google.auto.factory.Provided;
 import com.intellij.lang.java.JavaLanguage;
 import com.intellij.psi.*;
-import com.intellij.psi.impl.light.LightMethod;
-import com.intellij.psi.impl.light.LightModifierList;
-import com.intellij.psi.impl.light.LightPsiClassBase;
+import com.intellij.psi.impl.light.LightMethodBuilder;
+import com.intellij.psi.impl.light.LightPsiClassBuilder;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.PsiShortNamesCache;
+import mr.intellij.plugin.autofactory.utils.AnnotationUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
 
 /**
  * Finds AutoFactory-generated classes so that code using the factories will look valid, even before compiling.
@@ -25,85 +28,58 @@ public class AutoFactoryFinder extends PsiElementFinder {
             return null;
         }
 
-        // TODO Need to somehow internally pre-record all the @AutoFactory usages and not use this
+        // TODO build actual cache of @AutoFactory usages instead of finding the annotated class by name.
+        String factoryClassName = getClassName(qualifiedName);
+        String annotatedName = factoryClassName.substring(0, qualifiedName.length() - FACTORY_SUFFIX.length());
         PsiClass annotated = PsiShortNamesCache.getInstance(scope.getProject())
-                                               .getClassesByName(qualifiedName.substring(0, qualifiedName.length() - FACTORY_SUFFIX.length()), scope)[0];
-        String[] parts;
-        if (qualifiedName.contains(".")) {
-            parts = qualifiedName.split(".");
-        } else {
-            parts = new String[] {qualifiedName};
+                                               .getClassesByName(annotatedName, scope)[0];
+
+        LightPsiClassBuilder factoryClass = new LightPsiClassBuilder(annotated, factoryClassName);
+
+        Arrays.stream(annotated.getConstructors())
+              .filter(AnnotationUtils::hasAutoFactory)
+              .forEach(constructor -> addConstructor(factoryClass, constructor));
+
+        return factoryClass;
+    }
+
+    private void addConstructor(LightPsiClassBuilder factoryClass, PsiMethod constructor) {
+
+        LightMethodBuilder createBuilder = new LightMethodBuilder(factoryClass, JavaLanguage.INSTANCE)
+                .addModifier("public");
+
+        LightMethodBuilder constructorBuilder = new LightMethodBuilder(factoryClass, JavaLanguage.INSTANCE)
+                .setConstructor(true)
+                .addModifier("public");
+
+        for (PsiParameter parameter : constructor.getParameterList().getParameters()) {
+            if (AnnotationUtils.isAnnotationPresent(parameter.getModifierList(), Provided.class)) {
+                addParameterToMethod(parameter, constructorBuilder);
+            } else {
+                addParameterToMethod(parameter, createBuilder);
+            }
         }
-        String factoryClassName = parts[parts.length - 1];
 
-        return new LightPsiClassBase(annotated.getManager(), JavaLanguage.INSTANCE, factoryClassName) {
-            @NotNull
-            @Override
-            public PsiModifierList getModifierList() {
-                return new LightModifierList(getManager(), getLanguage(), "public");
-            }
+        factoryClass.addMethod(createBuilder);
+        factoryClass.addMethod(constructorBuilder);
+    }
 
-            @Nullable
-            @Override
-            public PsiReferenceList getExtendsList() {
-                // TODO
-                return null;
-            }
+    private String getClassName(@NotNull String qualifiedName) {
+        if (qualifiedName.contains(".")) {
+            String[] parts = qualifiedName.split(".");
+            return parts[parts.length - 1];
+        }
 
-            @Nullable
-            @Override
-            public PsiReferenceList getImplementsList() {
-                // TODO
-                return null;
-            }
+        return qualifiedName;
+    }
 
-            @NotNull
-            @Override
-            public PsiField[] getFields() {
-                return PsiField.EMPTY_ARRAY;
-            }
-
-            @NotNull
-            @Override
-            public PsiMethod[] getMethods() {
-                PsiElementFactory elementFactory = PsiElementFactory.SERVICE.getInstance(getProject());
-                return new PsiMethod[] {new LightMethod(getManager(), elementFactory.createMethod("create", elementFactory.createType(annotated)), this)};
-            }
-
-            @NotNull
-            @Override
-            public PsiClass[] getInnerClasses() {
-                return PsiClass.EMPTY_ARRAY;
-            }
-
-            @NotNull
-            @Override
-            public PsiClassInitializer[] getInitializers() {
-                return PsiClassInitializer.EMPTY_ARRAY;
-            }
-
-            @Override
-            public PsiElement getScope() {
-                return null;
-            }
-
-            @Nullable
-            @Override
-            public PsiClass getContainingClass() {
-                return null;
-            }
-
-            @Nullable
-            @Override
-            public PsiTypeParameterList getTypeParameterList() {
-                return null;
-            }
-        };
+    private void addParameterToMethod(PsiParameter parameter, LightMethodBuilder methodBuilder) {
+        methodBuilder.addParameter(parameter.getName(), parameter.getType());
     }
 
     @NotNull
     @Override
     public PsiClass[] findClasses(@NotNull String qualifiedName, @NotNull GlobalSearchScope scope) {
-        return PsiClass.EMPTY_ARRAY; // TODO -- needed when multiple class with the same are declared in different packages.
+        return PsiClass.EMPTY_ARRAY;
     }
 }
