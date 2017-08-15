f2cb38b8e26e63988a15fdece2a2b919af4af1e8
Matan Rosenberg <matan129@gmail.com>
2017-08-15 23:44:08 +0300

    Annotate AutoFactoryInspectionsProvider witn NonNull

--- src/main/java/mr/intellij/plugin/autofactory/inspections/AutoFactoryInspectionsProvider.java
+++ src/main/java/mr/intellij/plugin/autofactory/inspections/AutoFactoryInspectionsProvider.java
@@ -4,12 +4,14 @@ import com.intellij.codeInspection.InspectionToolProvider;
 import mr.intellij.plugin.autofactory.inspections.conflicting.constructor.ConflictingConstructorInspection;
 import mr.intellij.plugin.autofactory.inspections.direct.instantiation.DirectInstantiationInspection;
 import mr.intellij.plugin.autofactory.inspections.misusing.AutoFactoryOnClassAndConstructorInspection;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Provides IntelliJ with the plugin's custom inspections.
  */
 public class AutoFactoryInspectionsProvider implements InspectionToolProvider {
 
+    @NotNull
     @Override
     public Class[] getInspectionClasses() {
         return new Class[] {
